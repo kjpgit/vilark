2d777c88b940b5077c9d9625b05caf3bc145b565
kjpgit <kjpgit@users.noreply.github.com>
2023-09-08 11:45:04 -0500

    Test for all ascii chars

--- test/testchars.py
+++ test/testchars.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import os
 
 dirname="/tmp/testchars"
@@ -8,13 +8,18 @@ def make_file(n):
     path = dirname + "/" + n
     open(path, "w")
 
+ascii_tree = [chr(i) for i in range(32,127)]
+ascii_tree.remove('/')
+ascii_tree = "".join(ascii_tree)
+
 make_file("Combining    |\u0423\u0306|...")
 make_file("UTF8 Grin    |😬|...")
 make_file("UTF8 Smile   |😊|...")
 make_file("UTF8 Normal  |♠♥♦♣|")
 make_file("Super Long File " + ("X")*100 + " Ending Now.txt")
 
-# These files are probably working because ViLark returns them as ./<filename>
+# Chars that need to be escaped by vim fnameescape()
+make_file("ASCII XMAS TREE "+ ascii_tree + ".txt")
 make_file("<CR> File.txt")
 make_file("CR File 2<CR>.txt")
 make_file("+Plus File.txt")
