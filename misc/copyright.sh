1588e666c80b7f11f7db0638f93761789a2f13f9
kjpgit <kjpgit@users.noreply.github.com>
2023-09-09 17:59:50 -0500

    rename

---
+++ misc/copyright.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+set -o pipefail
+cd `dirname $0`
+
+COPYRIGHT='// Copyright (C) 2023 Karl Pickett / ViLark Project'
+
+process() {
+  while read filename; do
+    if ! head -n1 $filename | fgrep -q "$COPYRIGHT"; then
+       (echo "$COPYRIGHT"; cat $filename) > /tmp/file
+      mv /tmp/file $filename
+    fi
+  done
+}
+
+find ../src/ -name '*.cs' | process
