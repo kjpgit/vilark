4a6a0f6e038f49afbe46629a0e68cb4f1d388128
kjpgit <kjpgit@users.noreply.github.com>
2023-09-12 14:21:31 -0500

    add doc to the pstree.sh demo script

--- misc/pstree.sh
+++ misc/pstree.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
+#
+# This shows your process hierarchy, just so you
+# are aware of what's going on behind the scenes.  Example:
+#
+# bash
+#   vilark /usr    # Your main file explorer, supports IPC from vim
+#     vim          # Vim child process
+#       vilark     # Vilark (for buffer selection only)
+#
+# Usage:
+#   echo $BASHPID > /tmp/pid
+#   watch -n1 pstree.sh
 
-# echo $BASHPID > /tmp/pid
-# watch -n1 pstree.sh
 pstree -U -p --hide-threads -c -a `cat /tmp/pid`
 echo
 find /tmp -maxdepth 1 -name '*.vilarktmp'
