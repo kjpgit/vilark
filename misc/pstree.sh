bbb505ed23043d7e84838737e080e654c1838151
kjpgit <kjpgit@users.noreply.github.com>
2023-09-11 20:22:12 -0500

    s

--- misc/pstree.sh
+++ misc/pstree.sh
@@ -2,6 +2,6 @@
 
 # echo $BASHPID > /tmp/pid
 # watch -n1 pstree.sh
-pstree -U --hide-threads -c -a `cat /tmp/pid`
+pstree -U -p --hide-threads -c -a `cat /tmp/pid`
 echo
 find /tmp -maxdepth 1 -name '*.vilarktmp'
